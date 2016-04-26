952410ee873d639e860aabcbedbe6da51199c5ef
jimmy <jimmy@monstercat.com>
2016-04-26 12:01:54 -0700

    handle urls

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
-var spawn = require('child_process').spawn
-var tmp   = require('tmp')
-var async = require('async')
-var debug = require('debug')('image-combiner')
-var join  = require('path').join
+var spawn    = require('child_process').spawn
+var tmp      = require('tmp')
+var async    = require('async')
+var path     = require('path')
+var debug    = require('debug')('image-combiner')
+var join     = require('path').join
+var sizeOf   = require('image-size');
+var isUrl    = require('is-url')
+var request  = require('superagent')
 
 var types = {
   'image/text': processText,
   'image/image': processImage
 }
 
-function handle(data, done) {
+function downloadFiles(data, dir, done) {
+  var map = new Map()
+  async.each(data, (layer, cb)=> {
+    if (layer.file && isUrl(layer.file)) {
+      var filepath = path.join(dir, path.basename(layer.file))
+      var stream = fs.createWriteStream(filepath)
+      request.get(layer.file)
+      .pipe(stream)
+      .on('error', cb)
+      .on('finish', ()=> {
+        map.set(layer, filepath)
+        cb(null)
+      })
+    }
+    else if (layer.file) {
+      map.set(layer, layer.file)
+    }
+  }, err => {
+    done(err, map)
+  })
+}
+
+function handle(data, source, done) {
   if (!Array.isArray(data)) {
     data = [data]
   }
+  if (typeof source === 'function') {
+    done = source
+    source = null
+  }
 
-  tmp.dir((err, path, clean) => {
+  tmp.dir((err, filepath, clean) => {
     if (err) {
       debug('Error creating tmp file: %s', err)
       return done(err)
     }
-    var filepath = join(path, 'temp.png')
+    var filepath = join(filepath, 'temp.png')
 
-    async.eachSeries(data, processLayer.bind(null, filepath), err => {
-      debug('Output at %s', filepath)
-      done(err, filepath)
+    largestSize(data, (err, dim)=> {
+      if (err) {
+        debug('largestSize error: %s', err)
+        return done(err)
+      }
+      debug('dimensions %j', dim)
+
+      blank(filepath, dim.width, dim.height, err => {
+        if (err) {
+          debug('blank error: %s', err)
+          return done(err)
+        }
+        debug('Dimensions %dx%d', dim.width, dim.height)
+
+        async.eachSeries(data, processLayer.bind(null, filepath), err => {
+          debug('Output at %s', filepath)
+          done(err, filepath)
+        })
+      })
     })
   })
 }
 
-function processLayer(path, layer, done) {
+function processLayer(filepath, layer, done) {
   if (types[layer.type]) {
-    types[layer.type](path, layer, done)
+    types[layer.type](filepath, layer, done)
   }
   else {
     async.setImmediate(done)
   }
 }
 
-function processText(path, layer, done) {
+function processText(filepath, layer, done) {
   var args = []
     , x = layer.x || 0
     , y = layer.y || 0
@@ -52,19 +98,22 @@ function processText(path, layer, done) {
   args.push('-gravity', 'northwest')
   args.push('-annotate', `+${x}+${y}`, text)
   args.push('-fill', color)
-  args.push(path, path)
+  args.push(filepath, filepath)
   command('convert', args, done)
 }
 
-function processImage(path, layer, done) {
+function processImage(filepath, layer, done) {
   var args = []
     , x = layer.x || 0
     , y = layer.y || 0
-    , overlay = layer.overlay
+    , source = layer.file
 
+  if (!source) {
+    return done(Error('processImage: source not provided.'))
+  }
   args.push('-gravity', 'center')
   args.push('-geometry', `+${x}+${y}`)
-  args.push(overlay, path, path)
+  args.push(source, filepath, filepath)
   command('composite', args, done)
 }
 
@@ -87,6 +136,34 @@ function command(cmd, args, done) {
       debug(err)
     }
     return done(err)
+  })
+}
+
+function blank(filepath, w, h, done) {
+  var args = ['-size', `${w}x${h}`, 'xc:none', filepath]
+  command('convert', args, done)
+}
+
+function largestSize(layers, done) {
+  var dim = {
+    width: 0,
+    height: 0
+  }
+
+  async.eachSeries(layers, (layer, cb)=> {
+    if (!layer || layer.type !== 'image/image' || !layer.file) {
+      return cb(null)
+    }
+    sizeOf(layer.file, (err, d) => {
+      if (err) {
+        return cb(err)
+      }
+      dim.width = Math.max(dim.width, d.width)
+      dim.height = Math.max(dim.height, d.height)
+      cb(null)
+    })
+  }, err => {
+    done(err, dim)
   })
 }
 
